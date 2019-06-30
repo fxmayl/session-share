d1b5031ef5e08d3d658f4d5c49d84104281a2039
fxmayl <1318847212@qq.com>
2019-06-30 18:00:52 +0800

    添加

---
+++ client-1/src/test/java/com/my/binary/Solution.java
@@ -0,0 +1,95 @@
+package com.my.binary;
+
+/**
+ * @author : fangxiaoming
+ * @program : session-share
+ * @description : TODO
+ * @date : 2019-06-30 15:01
+ **/
+public class Solution {
+    public  static int numberOfOne(int n) {
+        StringBuffer bin = new StringBuffer();
+        if (n == Integer.MIN_VALUE) {
+            return 1;
+        }
+
+        int b = n;
+
+        if (n < 0) {
+            b = (-1) * n;
+        }
+
+
+        int k = b / 2;
+        int y = b % 2;
+        while (k != 0) {
+            bin.append(y);
+            y = k % 2;
+            k = k / 2;
+        }
+        bin.append(y);
+
+        char[] array = bin.toString().toCharArray();
+        int sum = 0;
+        if (n >= 0) {
+            for (int i = 0; i < array.length; i++) {
+                if ('1' == array[i]) {
+                    sum += 1;
+                }
+            }
+
+        } else {
+            char[] reserveArray = new char[32];
+            for (int i = 0; i < reserveArray.length; i++) {
+                if (i > array.length - 1) {
+                    reserveArray[i] = '1';
+                    continue;
+                }
+
+                if ('1' == array[i]) {
+                    reserveArray[i] = '0';
+                } else {
+                    reserveArray[i] = '1';
+                }
+            }
+
+
+            for (int i = 0; i < reserveArray.length; i++) {
+                if (reserveArray[i] == '0') {
+                    reserveArray[i] = '1';
+                    break;
+                } else {
+                    reserveArray[i] = '0';
+                }
+            }
+
+            for (int i = reserveArray.length - 1; i >= 0; i--) {
+                if (reserveArray[i] == '1') {
+                    sum += 1;
+                }
+            }
+        }
+        return sum;
+    }
+
+    public static double power(double base, int exponent) {
+        if (exponent == 1) {
+            return base;
+        }
+
+        if (exponent == 2) {
+            return base * base;
+        }
+        double sum = 1;
+        for (int i = 0; i < exponent; i++) {
+            sum = sum * base;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(numberOfOne(-1));
+
+        System.out.println(power(2,3));
+    }
+}
