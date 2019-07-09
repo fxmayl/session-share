faf4cfc0f0f09b511e10472e4eec8647bf5bdc5e
fxm <fang17357>
2019-07-09 11:22:48 +0800

    新增

--- client-1/src/test/java/com/my/treepath/Solution.java
+++ client-1/src/test/java/com/my/treepath/Solution.java
@@ -1,8 +1,6 @@
 package com.my.treepath;
 
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * @author : fangxiaoming
@@ -11,6 +9,50 @@ import java.util.Map;
  * @date : 2019-07-07 13:12
  **/
 public class Solution {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        TreeNode root = new TreeNode(10);
+        root.left = new TreeNode(5);
+        root.right = new TreeNode(12);
+        root.left.left = new TreeNode(4);
+        root.left.right = new TreeNode(7);
+        System.out.println(solution.findPath(root, 22));
+    }
+
+    public ArrayList<ArrayList<Integer>> findPath(TreeNode root, int target) {
+        ArrayList<ArrayList<Integer>> arr = new ArrayList<>();
+        if (root == null) {
+            return arr;
+        }
+        ArrayList<Integer> a1 = new ArrayList<>();
+        int sum = 0;
+        path(root, target, arr, a1, sum);
+        return arr;
+    }
+
+    private void path(TreeNode root, int target, ArrayList<ArrayList<Integer>> arr,
+        ArrayList<Integer> a1, int sum) {
+        if (root == null) {
+            return;
+        }
+        sum += root.val;
+
+        if (root.left == null && root.right == null) {
+            if (sum == target) {
+                a1.add(root.val);
+                arr.add(new ArrayList<>(a1));
+                a1.remove(a1.size() - 1);
+
+            }
+            return;
+        }
+
+        a1.add(root.val);
+        path(root.left, target, arr, a1, sum);
+        path(root.right, target, arr, a1, sum);
+        a1.remove(a1.size() - 1);
+    }
+
     public static class TreeNode {
         int val = 0;
         TreeNode left = null;
@@ -21,40 +63,5 @@ public class Solution {
 
         }
 
-    }
-
-    public ArrayList<ArrayList<Integer>> findPath(TreeNode root, int target) {
-        if (root == null) {
-            return new ArrayList<>();
-        }
-
-        ArrayList<ArrayList<Integer>> result = new ArrayList<>();
-
-        path(root, root.val);
-
-        return result;
-    }
-
-    private void path(TreeNode root, int current) {
-        if (root == null) {
-            return;
-        }
-        root.val = current;
-        if (root.left != null) {
-            path(root.left, current + root.left.val);
-        }
-        if (root.right != null) {
-            path(root.right, current + root.right.val);
-        }
-    }
-
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-        TreeNode root = new TreeNode(10);
-        root.left = new TreeNode(5);
-        root.right = new TreeNode(12);
-        root.left.left = new TreeNode(4);
-        root.left.right = new TreeNode(7);
-        solution.findPath(root, 22);
     }
 }
