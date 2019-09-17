5954e5762d9730ec686f4aa78cc3a0372cc7c8ae
fxm <fang17357>
2019-09-17 19:36:40 +0800

    修改

--- client-1/src/test/java/com/my/socket/Response.java
+++ client-1/src/test/java/com/my/socket/Response.java
@@ -30,21 +30,24 @@ public class Response {
 
         try {
             File file = new File(HttpServer.WEB_ROOT, request.getUri());
+            String h = "HTTP/1.1 200 OK\r\n" + "Content-Type: text/html\r\n" + "\r\n";
+            byte[] hBytes = h.getBytes();
             if (file.exists()) {
-                bytes = new byte[Integer.parseInt(String.valueOf(file.length()))];
+                bytes = new byte[Integer.parseInt(String.valueOf(file.length())) + hBytes.length];
+                for (int i = 0; i < hBytes.length; i++) {
+                    bytes[i] = hBytes[i];
+                }
                 fis = new FileInputStream(file);
-                int ch = fis.read(bytes, 0, Integer.parseInt(String.valueOf(file.length())));
-                while (ch != -1) {
-                    output.write(bytes, 0, ch);
-                    ch = fis.read(bytes, 0, Integer.parseInt(String.valueOf(file.length())));
+                int len = fis.read(bytes, hBytes.length, Integer.parseInt(String.valueOf(file.length())));
+                while (len != -1) {
+                    output.write(bytes, 0, len);
+                    len = fis.read(bytes, 0, Integer.parseInt(String.valueOf(file.length())));
                 }
                 System.out.println(new String(bytes));
             } else {
-                String errorMessage = "HTTP/1.1 404 File Not Found\r\n" +
-                        "Content-Type: text/html\r\n" +
-                        "Content-Length: 23\r\n" +
-                        "\r\n" +
-                        "<h1>File Not Found</h1>";
+                String errorMessage =
+                    "HTTP/1.1 404 File Not Found\r\n" + "Content-Type: text/html\r\n" +
+                        "Content-Length: 23\r\n" + "\r\n" + "<h1>File Not Found</h1>";
                 output.write(errorMessage.getBytes());
             }
         } catch (Exception e) {
